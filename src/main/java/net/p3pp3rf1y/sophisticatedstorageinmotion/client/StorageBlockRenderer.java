2212da10547aee20654f7e9d83c4485a05d2595c
Salandora <salandora@gmail.com>
2025-07-06 15:39:43 +0200

    [Chore]: Added SophisticatedLibrary module

--- src/main/java/net/p3pp3rf1y/sophisticatedstorageinmotion/client/StorageBlockRenderer.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorageinmotion/client/StorageBlockRenderer.java
@@ -1,5 +1,6 @@
 package net.p3pp3rf1y.sophisticatedstorageinmotion.client;
 
+import com.github.salandora.sophisticatedlibrary.model.util.ModelData;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Minecraft;
@@ -13,7 +14,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
-import net.p3pp3rf1y.sophisticatedcore.util.model.ModelData;
 import net.p3pp3rf1y.sophisticatedstorage.block.BarrelBlockEntity;
 import net.p3pp3rf1y.sophisticatedstorage.block.StorageBlockEntity;
 import net.p3pp3rf1y.sophisticatedstorage.client.render.BarrelBakedModelBase;
